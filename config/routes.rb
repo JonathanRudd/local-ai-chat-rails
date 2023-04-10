7d77fa9f200bf4344564ac4f032b39d820131731
Jonathan Rudd <jonathanrudd3@gmail.com>
2023-04-10 23:45:09 +0900

    pasted basic home page, need env and gpt api gems now

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Generic syntax:
+  # verb "path", to: "controller#action"
+  root to: "pages#home"
 end
