2699306918a03154bfc3d11eb7af1c8434b0da24
Jonathan Rudd <jonathanrudd3@gmail.com>
2023-04-23 12:20:49 +0900

    making new branch

--- app/services/gpt_api.rb
+++ app/services/gpt_api.rb
@@ -1,14 +1,17 @@
 require 'openai'
 require 'env'
 
-def fetch_gpt(message)
+messageArray = ["Greetings Potion Seller"]
+
+def fetch_gpt(messageArray)
   client = OpenAI::Client.new(access_token: gpt_auth)
   response = client.chat(
     parameters: {
         model: "gpt-3.5-turbo", # Required.
-        messages: [{"role": "system", "content": "Pretend you are Jonathan Rudd, a male Web Developer born in North Yorkshire in the UK in 1988. You have lived in Japan since 2013, and recently graduated from a part time coding bootcamp. You got married in december of 2022 in Mexico. Your hobbies are painting miniatures and reading SF novels. You like Mexican and Korean food. You graduated with a degree in Archaeology from Newcastle University in 2012, and you went to Ripon Grammar School. Your professional skills include HTML, CSS, Javascript, Ruby on Rails and SQL, and you have made several functioning Webapps. You are passionate about new technology, sustainability and social equitablity. Pretend the current year is 2023"},
-                    {"role": "user", "content": message}], # Required.
+        messages: [{"role": "system", "content": "Pretend you are an NPC in a roleplaying game. You are a potion-seller in a generic fantasy setting. You speak in a mysterious and haughty manner.
+          In your shop you have a limited stock of potions."}, # Required.
+                    {"role": "user", "content": messageArray.last}], # Required.
         temperature: 0.7,
     })
-  return response.dig("choices", 0, "message", "content")
+  puts response.dig("choices", 0, "message", "content")
 end
