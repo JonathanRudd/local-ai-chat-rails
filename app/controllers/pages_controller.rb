50d5e53b10eff35036eb477f2149a6304a0acefc
Jonathan Rudd <jonathanrudd3@gmail.com>
2023-04-28 22:59:12 +0900

    put gpt_fetch function in class

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,33 @@ class PagesController < ApplicationController
     @title = "Home"
 
   end
+
+  require 'openai'
+
+
+def fetch_gpt(messageArray)
+  client = OpenAI::Client.new(access_token: ENV.fetch('OPENAI_ACCESS_TOKEN'))
+
+  messages = [{ "role": "system", "content": "Pretend you are an NPC in a roleplaying game. You are a potion-seller in a generic fantasy setting. You speak in a mysterious and haughty manner.
+    In your shop you have a limited stock of potions." }]
+  messageArray.each do |message|
+    messages << message
+  end
+
+  p messages
+
+  response = client.chat(
+    parameters: {
+        model: "gpt-3.5-turbo", # Required.
+        messages: messages,
+        temperature: 0.7,
+    })
+  puts response.dig("choices", 0, "message", "content")
+  console.log response.dig("choices", 0, "message", "content")
+end
+
+
+
+
+
 end
--- app/services/gpt_api.rb
+++ app/services/gpt_api.rb
@@ -3,6 +3,28 @@ require 'openai'
 # currently empty class for naming purposes, will move methods here later
 class GptApi
 
+  def fetch_gpt(messageArray)
+    client = OpenAI::Client.new(access_token: ENV.fetch('OPENAI_ACCESS_TOKEN'))
+
+    messages = [{ "role": "system", "content": "Pretend you are an NPC in a roleplaying game. You are a potion-seller in a generic fantasy setting. You speak in a mysterious and haughty manner.
+      In your shop you have a limited stock of potions." }]
+    messageArray.each do |message|
+      messages << message
+    end
+
+    p messages
+
+    response = client.chat(
+      parameters: {
+          model: "gpt-3.5-turbo", # Required.
+          messages: messages,
+          temperature: 0.7,
+      })
+    puts response.dig("choices", 0, "message", "content")
+  end
+
+
+
 end
 
 # def client
@@ -15,23 +37,3 @@ end
 
 # example:
 convoArray = [{ "role": "user", "content": "Greetings Potion Seller" }, { "role": "assistant", "content": "Welcome, adventurer. I am the potion seller, purveyor of the finest elixirs in all the land. What brings you to my humble shop today?" }, { "role": "user", "content": "I am in need of a potion of healing"} ]
-
-def fetch_gpt(messageArray)
-  client = OpenAI::Client.new(access_token: ENV.fetch('OPENAI_ACCESS_TOKEN'))
-
-  messages = [{ "role": "system", "content": "Pretend you are an NPC in a roleplaying game. You are a potion-seller in a generic fantasy setting. You speak in a mysterious and haughty manner.
-    In your shop you have a limited stock of potions." }]
-  messageArray.each do |message|
-    messages << message
-  end
-
-  p messages
-
-  response = client.chat(
-    parameters: {
-        model: "gpt-3.5-turbo", # Required.
-        messages: messages,
-        temperature: 0.7,
-    })
-  puts response.dig("choices", 0, "message", "content")
-end
