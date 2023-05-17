06d3349005e2271eb3e29c6646199b3e1a51b841
Jonathan Rudd <jonathanrudd3@gmail.com>
2023-05-17 19:11:16 +0900

    finally returned text only response, very stupid error :´(

--- app/services/gpt_api.rb
+++ app/services/gpt_api.rb
@@ -2,30 +2,24 @@ require 'openai'
 require 'httparty'
 
 class GptApi
-  # include HTTParty
   def fetch_gpt(messageArray)
     client = OpenAI::Client.new(access_token: ENV.fetch('OPENAI_ACCESS_TOKEN'))
 
-    messages = [{ "role": "system", "content": "Pretend you are an NPC in a roleplaying game. You are a potion-seller in a generic fantasy setting. You speak in a mysterious and haughty manner.
-      In your shop you have a limited stock of potions." }]
+    messages = [{ "role": "system", "content": "Pretend you are an NPC in a roleplaying game. You are a potion-seller in a generic fantasy setting. You speak in a mysterious and haughty manner. In your shop you have a limited stock of potions." }]
     messageArray.each do |message|
       messages << message
     end
 
-    # p messages
     response = client.chat(
       parameters: {
           model: "gpt-3.5-turbo", # Required.
           messages: messages,
           temperature: 0.7,
       })
-    # puts response.dig("choices", 0, "message", "content")
 
-    response['choices'][-1]["message"]["content"]
-    # this returns the last message in the array, but also the first message in the array is the prompt
-    # so I need to remove the first message from the array by using .shift
+response['choices'][-1]["message"]["content"]
+
   end
-
 end
 
 
